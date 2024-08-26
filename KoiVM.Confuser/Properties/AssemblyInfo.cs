7058c275cb7a92b6cb7c00c8f463e45f01eebf3b
Andrii Kurdiumov <kant2002@gmail.com>
2024-08-26 12:07:03 +0500

    Remove duplicate projects and use KoiVM from submodule

--- KoiVM.Confuser/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿#region
-
-using System.Reflection;
-
-#endregion
-
-[assembly: AssemblyTitle("KoiVM Confuser")]
-[assembly: AssemblyDescription("KoiVM Confuser Interface")]
--- KoiVM.Runtime/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿#region
-
-using System.Reflection;
-
-#endregion
-
-[assembly: AssemblyTitle("KoiVM Runtime")]
-[assembly: AssemblyDescription("KoiVM runtime library")]
