a2d3dcab8b887dc705b6c74bf3c933b5a413a718
Andrii Kurdiumov <kant2002@gmail.com>
2024-08-26 11:28:53 +0500

    Add origin source for grammatica-1.5 file

--- BuildGrammer.bat
+++ BuildGrammer.bat
@@ -1,3 +1,4 @@
 @echo off
+REM grammatica-1.5.jar is from https://github.com/cederberg/grammatica/releases/download/v1.5/grammatica-1.5.zip
 java -jar libs/grammatica-1.5.jar docs/IRGrammer.txt --csoutput KoiVM/VMIR/Compiler --csnamespace KoiVM.VMIR.Compiler --csclassname IR
 pause
